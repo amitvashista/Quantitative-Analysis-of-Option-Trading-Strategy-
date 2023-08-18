a6b982a70efce16d92d1b4c8afdaa2790b8b8586
amitvashista <64914226+amitvashista@users.noreply.github.com>
2023-08-18 21:49:04 +0530

    Update trading_strategy.py

--- trading_strategy.py
+++ trading_strategy.py
@@ -2,14 +2,16 @@ import numpy as np
 
 pro=[]
 ln=10000
+day=5
 for j in range(ln):
     initial_investment=10000
-    for i in range(5):
+    #for 5 day 
+    for i in range(day):
         # 0.3 is my target price and -0.3 is my stop loss
-        # 0.6 is winning probability of my strategy and 0.4 is loss probability
-        # i get winning probability from paper trade or from privious trade using certain strategy, 
-        # like i get 6 winning trade out of 10 then my winning probability is 0.6%
-        x=np.random.choice([0.3,-0.3],p=[0.34,0.66])   
+        # 0.6 is winning probability of the strategy and 0.4 is loss probability
+        # get winning probability from paper trade or from privious trade using certain strategy, 
+        # like if you get 6 winning trade out of 10 then my winning probability is 0.6%
+        x=np.random.choice([0.3,-0.3],p=[0.6,0.4])   
 
         # lot_price is price of 1 lot of bank nifty
         # uniform(4000,8000) means that lot price can be anything in between 4000 to 8000
@@ -27,7 +29,7 @@ for j in range(ln):
 
 
 
-z=[i for i in pro if i >11000]  #give no. of trade close more then 11000 out of 100000
+z=[i for i in pro if i >10000]  #give no. of trade close more then 10000 out of 10000
 print('profit probability = ',(len(z)/ln)*100)
 print('median : ',np.median(pro))
 print('mean : ',np.mean(pro))
